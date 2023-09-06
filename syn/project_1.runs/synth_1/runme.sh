cf2050fcff2bce7a7cebb5f400375e47e21eb7ae
Omar <write2faruque@gmail.com>
2023-09-06 14:31:22 -0600

    fixes

--- syn/project_1.runs/synth_1/runme.sh
+++ syn/project_1.runs/synth_1/runme.sh
@@ -25,7 +25,7 @@ else
 fi
 export LD_LIBRARY_PATH
 
-HD_PWD='C:/Users/write/Downloads/MIPS_Project/syn/project_1/project_1.runs/synth_1'
+HD_PWD='C:/Users/write/Downloads/MIPS_Project/syn/project_1.runs/synth_1'
 cd "$HD_PWD"
 
 HD_LOG=runme.log
--- syn/project_1.runs/synth_1/top.tcl
+++ syn/project_1.runs/synth_1/top.tcl
@@ -4,7 +4,7 @@
 
 set TIME_start [clock seconds] 
 namespace eval ::optrace {
-  variable script "C:/Users/write/Downloads/MIPS_Project/syn/project_1/project_1.runs/synth_1/top.tcl"
+  variable script "C:/Users/write/Downloads/MIPS_Project/syn/project_1.runs/synth_1/top.tcl"
   variable category "vivado_synth"
 }
 
@@ -70,31 +70,25 @@ proc create_report { reportName command } {
   }
 }
 OPTRACE "synth_1" START { ROLLUP_AUTO }
-set_param checkpoint.writeSynthRtdsInDcp 1
-set_param chipscope.maxJobs 5
-set_param synth.incrementalSynthesisCache C:/Users/write/AppData/Roaming/Xilinx/Vivado/.Xil/Vivado-20876-LAPTOP-GU88T9Q9/incrSyn
-set_msg_config -id {HDL-1065} -limit 10000
-set_msg_config -id {Synth 8-256} -limit 10000
-set_msg_config -id {Synth 8-638} -limit 10000
 OPTRACE "Creating in-memory project" START { }
 create_project -in_memory -part xc7z010clg400-1
 
 set_param project.singleFileAddWarning.threshold 0
 set_param project.compositeFile.enableAutoGeneration 0
 set_param synth.vivado.isSynthRun true
-set_property webtalk.parent_dir C:/Users/write/Downloads/MIPS_Project/syn/project_1/project_1.cache/wt [current_project]
-set_property parent.project_path C:/Users/write/Downloads/MIPS_Project/syn/project_1/project_1.xpr [current_project]
+set_property webtalk.parent_dir C:/Users/write/Downloads/MIPS_Project/syn/project_1.cache/wt [current_project]
+set_property parent.project_path C:/Users/write/Downloads/MIPS_Project/syn/project_1.xpr [current_project]
 set_property default_lib xil_defaultlib [current_project]
 set_property target_language Verilog [current_project]
 set_property board_part digilentinc.com:zybo-z7-10:part0:1.1 [current_project]
-set_property ip_repo_paths c:/Users/write/Downloads/MIPS_Project/syn/project_1 [current_project]
+set_property ip_repo_paths c:/Users/write/Downloads/MIPS_Project/syn [current_project]
 update_ip_catalog
-set_property ip_output_repo c:/Users/write/Downloads/MIPS_Project/syn/project_1/project_1.cache/ip [current_project]
+set_property ip_output_repo c:/Users/write/Downloads/MIPS_Project/syn/project_1.cache/ip [current_project]
 set_property ip_cache_permissions {read write} [current_project]
 OPTRACE "Creating in-memory project" END { }
 OPTRACE "Adding files" START { }
-read_mem C:/Users/write/Downloads/MIPS_Project/syn/mips_cpu/inst_data.mem
-read_verilog -library xil_defaultlib C:/Users/write/Downloads/MIPS_Project/syn/mips_cpu/MIPS_Processor.v
+read_mem C:/Users/write/Downloads/MIPS_Project/syn/inst_data.mem
+read_verilog -library xil_defaultlib C:/Users/write/Downloads/MIPS_Project/syn/MIPS_Processor.v
 OPTRACE "Adding files" END { }
 # Mark all dcp files as not used in implementation to prevent them from being
 # stitched into the results of this synthesis run. Any black boxes in the
@@ -106,7 +100,7 @@ foreach dcp [get_files -quiet -all -filter file_type=="Design\ Checkpoint"] {
 }
 set_param ips.enableIPCacheLiteLoad 1
 
-read_checkpoint -auto_incremental -incremental C:/Users/write/Downloads/MIPS_Project/syn/project_1/project_1.srcs/utils_1/imports/synth_1/top.dcp
+read_checkpoint -auto_incremental -incremental C:/Users/write/Downloads/MIPS_Project/syn/project_1.srcs/utils_1/imports/synth_1/top.dcp
 close [open __synthesis_is_running__ w]
 
 OPTRACE "synth_design" START { }
